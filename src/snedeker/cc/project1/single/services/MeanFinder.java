1c1c7190a4538870c9b6ba80f928dc82e28fc77c
Colby Snedeker <Woden501@gmail.com>
2016-02-23 15:44:04 -0500

    Modified to properly do a running SMA.

--- src/snedeker/cc/project1/single/services/MeanFinder.java
+++ src/snedeker/cc/project1/single/services/MeanFinder.java
@@ -7,6 +7,7 @@ import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
@@ -21,7 +22,7 @@ import org.apache.commons.io.FileUtils;
  */
 public class MeanFinder {
 	
-	TreeMap<String, TreeMap<Date, Double>> companies;
+	TreeMap<String, TreeMap<Long, Double>> companies;
 	
 	/**
 	 * Default constructor.  Simply instantiates the companies TreeMap.
@@ -45,7 +46,7 @@ public class MeanFinder {
 		for (String entry : entries) {
 			// [0] - company, [1] - date, [2] - value
 			String[] values = entry.split(",");
-			TreeMap<Date, Double> dateValues = companies.get(values[0]);
+			TreeMap<Long, Double> dateValues = companies.get(values[0]);
 			
 			if (dateValues != null) {
 				// Add date and value pair to company's entry
@@ -68,11 +69,11 @@ public class MeanFinder {
 	 * @param dateValues The TreeMap that contains a company's date/price pairs.
 	 * @throws ParseException The date String is not valid.
 	 */
-	private void addDateValue(String[] values, TreeMap<Date, Double> dateValues) throws ParseException {
+	private void addDateValue(String[] values, TreeMap<Long, Double> dateValues) throws ParseException {
 		DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
 		format.setLenient(false);
 		Date date = format.parse(values[1]);
-		dateValues.put(date, Double.parseDouble(values[2]));
+		dateValues.put(date.getTime(), Double.parseDouble(values[2]));
 	}
 
 	/**
@@ -81,22 +82,49 @@ public class MeanFinder {
 	 * @param windowSize The number of past dates to take the average of.
 	 */
 	public void calculateMeans(int windowSize) {
-		for (Map.Entry<String, TreeMap<Date, Double>> entry : companies.entrySet()) {
+		for (Map.Entry<String, TreeMap<Long, Double>> entry : companies.entrySet()) {
 			int entryCount = 0;
-			double total = 0;
+			LinkedList<Double> prices = new LinkedList<>();
 			
-			for (Map.Entry<Date, Double> dateValue : entry.getValue().descendingMap().entrySet()) {
+			// Iterate through each date/value pair for the company calculating the running mean
+			for (Map.Entry<Long, Double> dateValue : entry.getValue().entrySet()) {
+				
+				double total = 0;
+				
+				entryCount++;
+				
+				if (prices.size() >= windowSize)
+					prices.remove();
+				
+				prices.add(dateValue.getValue());
+				
+				double mean = 0;
+				
 				if (entryCount < windowSize) {
-					++entryCount;
-					total += dateValue.getValue();
+					for (int i = 0; i < entryCount; i++) {
+						total += prices.get(i);
+					}
+					
+					mean = total / (double) entryCount;
 				}
+				else {
+					for (int i = 0; i < windowSize; i++) {
+						total += prices.get(i);
+					}
+					
+					mean = total / (double) windowSize;
+				}
+
+				DecimalFormat df = new DecimalFormat("#.00");
+				
+				SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+				String date = dateFormatter.format(new Date(dateValue.getKey()));
+				
+				System.out.println(entry.getKey() + "," + date + "," + df.format(mean));
 			}
 			
-			double mean = total / (double) entryCount;
 			
-			DecimalFormat df = new DecimalFormat("#.00");
-			
-			System.out.println("Company: " + entry.getKey() + ", Mean Value: " + df.format(mean));
+//			System.out.println("Company: " + entry.getKey() + ", Mean Value: " + df.format(mean));
 		}
 	}
 }
